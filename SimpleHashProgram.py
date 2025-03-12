36bd2df0362ce70017cb50332423f393438c3edf
5115-Source <chegstad5115@student.whatcom.edu>
2025-03-11 22:02:30 -0700

    SimpleHashMap program finished with basic comments

--- SimpleHashProgram.py
+++ SimpleHashProgram.py
@@ -1,23 +1,21 @@
 #In python the use of the built in dict class is useful for working with hash maps
-
-
 def main():
-    #Create a hash map
+    #Create a hash map, add some values to it, and print the initial state of the hash map
     hashMap = dict()
-    #Add some key value pairs to the hash map
     hashMap = {0:"bananna", 1:"tomato", 2:"apple", 3:"pear", 4:"orange", 5:"guava", 6:"mango", 7:"kiwi", 8:"pomegranate", 9:"pineapple"}
-    #Print all entries in the hashMap
     print("Initial HashMap Contents: ", hashMap)
 
     userInput(hashMap)
     
+#Ask user for input to edit the hash map in different ways
 def userInput(hashMap):
     #User input without any error checking because this is just a sample program using a hash map
     key = int(input("Enter the key (integer) to search for a fruit: "))
     print("The fruit corresponding to the key", key, "is: ", hashMap[key] )
     
+    #Remove a given key from the hash map and print the resulting hash map
     removalKey = int(input("Enter the key to remove from the HashMap: "))
-    
+    hashMap.pop(removalKey)
     print("After removing key", removalKey, "the HashMap contents are: ", hashMap)
     
 #If there is a main method, run it
